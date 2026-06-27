df619b9ec8a8d03cec905d490cba7dea642403c1
dhvanitmonpara <monparadhvanit@gmail.com>
2026-06-27 12:37:54 +0530

    basic server setup

---
+++ cmd/url-shortner/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"context"
+	"log"
+	"log/slog"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+	"url-shortner/internal/config"
+)
+
+func main() {
+
+	cfg := config.MustLoad()
+
+	router := http.NewServeMux()
+
+	// router.HandleFunc()
+
+	server := http.Server{
+		Addr:    cfg.HttpServer.Addr,
+		Handler: router,
+	}
+
+	slog.Info("server started", slog.String("address", cfg.Addr))
+
+	done := make(chan os.Signal, 1)
+
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		err := server.ListenAndServe()
+		if err != nil {
+			log.Fatal("failed to start server")
+		}
+	}()
+
+	<-done
+
+	slog.Info("shutting down the server")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := server.Shutdown(ctx); err != nil {
+		slog.Error("failed to shutdown server", slog.String("error", err.Error()))
+	}
+
+	slog.Info("server shutdown successfully")
+
+}
